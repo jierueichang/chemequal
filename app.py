331e6b4e5aef0c89242a0887c946835216ff369c
jierueichang <47700551+jierueichang@users.noreply.github.com>
2020-02-10 16:39:09 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,12 +3,14 @@ import chemequal
 
 app = Flask(__name__)
 
+def escape(eq):
+    return eq.replace('>','&gt;').replace('<','&lt;')
+
 @app.route('/',methods=['GET','POST'])
 def main():
     if request.method=='POST':
-        eq1 = request.form['eq1']
-        eq2 = request.form['eq2']
-        print(eq1)
+        eq1 = escape(request.form['eq1'])
+        eq2 = escape(request.form['eq2'])
         res = chemequal.run(eq1,eq2)
         print(res)
         return render_template('index.html',ans = res)
